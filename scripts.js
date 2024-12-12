90633847dc26f683349f58aa451d33eaf3f44935
WaltFredricks <walt.fredricks1776@gmail.com>
2024-12-12 07:59:06 -0500

    script update

--- scripts.js
+++ scripts.js
@@ -7,6 +7,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18
 }).addTo(map);
 
+// Function to create custom icons
+function createIcon(iconUrl) {
+    return L.icon({
+        iconUrl: iconUrl,
+        iconSize: [25, 41], // Adjust size if necessary
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+}
+
 // Fetch locations from locs.json and add them to the map
 fetch('https://waltfredricks.github.io/locs.json')
     .then(response => {
@@ -18,7 +29,30 @@ fetch('https://waltfredricks.github.io/locs.json')
     .then(locations => {
         const bounds = [];
         locations.forEach(loc => {
-            const marker = L.marker([loc.lat, loc.lon]).addTo(map);
+            // Choose the appropriate icon based on the entity type
+            let icon;
+            switch (loc.entity.toLowerCase()) {
+                case 'army':
+                    icon = createIcon('https://home.army.mil/imcom/cache/thumbnails/42c949ce277ff0c3b6b24416935b117c.png'); // Army icon
+                    break;
+                case 'police':
+                    icon = createIcon('https://home.army.mil/imcom/cache/thumbnails/c90d3b2ec235df2ab47e9c5d6a01e069.png'); // Police icon
+                    break;
+                case 'native':
+                    icon = createIcon('https://home.army.mil/imcom/cache/thumbnails/4da4933bf240621a4ccc60fde8faf75c.png'); // Native icon
+                    break;
+                default:
+                    icon = L.icon({ // Default Leaflet marker
+                        iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+                        iconSize: [25, 41],
+                        iconAnchor: [12, 41],
+                        popupAnchor: [1, -34],
+                        shadowSize: [41, 41]
+                    });
+            }
+
+            // Add marker with custom icon
+            const marker = L.marker([loc.lat, loc.lon], { icon: icon }).addTo(map);
             marker.bindPopup(`
                 <div style="font-size: 14px;">
                     <strong>${loc.name}</strong><br>
